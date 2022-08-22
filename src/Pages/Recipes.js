d3cb6585aafd8075db44dd2f05f220c1ee347c5f
Victor <matheus0588@outlook.com>
2022-08-22 17:00:06 -0300

    Feito req10

--- src/Pages/Recipes.js
+++ src/Pages/Recipes.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import Header from '../Components/Header';
+import SearchBar from '../Components/SearchBar.js';
 
 function Recipes() {
   return (
     <main>
       <Header pageName="Foods" />
+      <SearchBar />
       <h1>Hello Recipes</h1>
     </main>
   );
