80900125342def1c41ccdf2cfa0691c75a52f9fa
Gabriel Garcia <philiasmaximus@gmail.com>
2022-08-23 16:06:31 -0300

    req 57 ao 59

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -3,12 +3,37 @@ import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 
 function Profile() {
+  const userEmail = JSON.parse(localStorage.getItem('user'));
   return (
     <>
       <Header pageName="Profile" search={ false } />
-      <div>
-        <Footer />
-      </div>
+      <p data-testid="profile-email">
+        Email:
+        { userEmail.email }
+      </p>
+      <button
+        type="button"
+        name="done"
+        data-testid="profile-done-btn"
+      >
+        Done Recipes
+
+      </button>
+      <button
+        type="button"
+        data-testid="profile-favorite-btn"
+      >
+        Favorite Recipes
+
+      </button>
+      <button
+        type="button"
+        data-testid="profile-logout-btn"
+      >
+        Logout
+
+      </button>
+      <Footer />
     </>
   );
 }
