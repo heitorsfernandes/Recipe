a8972bdbdb053dd5c503fc6cb5c4173889ef211a
HeitorFernandes <heitorsoaresfer@gmail.com>
2022-08-23 18:50:15 -0300

    Requisitos 14 e 15(no test)

--- src/Pages/RecipesDrinks.js
+++ src/Pages/RecipesDrinks.js
@@ -2,13 +2,14 @@ import React from 'react';
 import Footer from '../Components/Footer';
 import Header from '../Components/Header';
 import SearchBar from '../Components/SearchBar.js';
+import SearchCards from '../Components/SearchCards';
 
 function RecipesDrinks() {
   return (
     <main>
       <Header pageName="Drinks" />
       <SearchBar pageName="drinks" />
-      <h1>Hello Recipes</h1>
+      <SearchCards />
       <Footer />
     </main>
   );
