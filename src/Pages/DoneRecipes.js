4eace358cbe392cd7fd1493996a6b34aaa3b1f34
Marcos Leite <marcosvpleite@gmail.com>
2022-08-25 17:15:13 -0300

    requisito 44

--- src/Pages/DoneRecipes.js
+++ src/Pages/DoneRecipes.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import CardDoneRecipes from '../Components/CardDoneRecipes';
 import Header from '../Components/Header';
 
 function DoneRecipes() {
   return (
-    <Header pageName="Done Recipes" search={ false } />
+    <div>
+      <section>
+        <Header pageName="Done Recipes" search={ false } />
+        <button
+          type="button"
+          name="all"
+          data-testid="filter-by-all-btn"
+        >
+          All
+        </button>
+        <button
+          type="button"
+          name="Food"
+          data-testid="filter-by-food-btn"
+        >
+          Food
+        </button>
+        <button
+          type="button"
+          name="Drinks"
+          data-testid="filter-by-drink-btn"
+        >
+          Drinks
+        </button>
+      </section>
+      <section>
+        <CardDoneRecipes />
+      </section>
+    </div>
   );
 }
 
